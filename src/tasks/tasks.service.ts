1bbffdaf2039ddb2b09804d3a47cf8f87367d75f
turakijnr <btnuuman@gmail.com>
2022-02-21 01:35:54 +0100

    task dto added

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,14 +1,24 @@
-import {Injectable} from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
 import {Task, TaskStatus} from "./task.model";
 import {v1 as uuidv1} from 'uuid';
+import {CreateTaskDto} from "./dto/create-task.dto";
 
 @Injectable()
 export class TasksService {
     private  tasks: Task[] = [];
+
     getAllTasks(): Task[]{
         return this.tasks
     }
-    createTask(title: string ,description : string): Task{
+    getTaskById(id : string): Task{
+       const found =  this.tasks.find(task=> task.id === id)
+        if(!found){
+            throw new NotFoundException()
+        }
+        return found
+    }
+    createTask(createTaskDTO: CreateTaskDto): Task{
+        const {title, description}= createTaskDTO
         const task: Task = {
             id: uuidv1(),
             title,
@@ -17,5 +27,8 @@ export class TasksService {
         }
         this.tasks.push(task)
         return task
+    }
+    deleteTask(id : string): void{
+       this.tasks = this.tasks.filter(task=> task.id === id)
     }
 }
